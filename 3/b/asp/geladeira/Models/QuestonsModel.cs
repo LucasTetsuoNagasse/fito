cb6769d46a72fa873d09f8ce15bdcff83f6fee60
Lucas Tetsuo Nagasse <109025053+LucasTetsuoNagasse@users.noreply.github.com>
2023-09-23 11:50:59 -0300

    Add files via upload

---
+++ 3/b/asp/geladeira/Models/QuestonsModel.cs
@@ -0,0 +1,15 @@
+namespace geladeira.Models;
+
+public class QuestonsModel
+{
+  public string? q1 { get; set; }
+  public string? q2 { get; set; }
+  public string? q3 { get; set; }
+  public string? q4 { get; set; }
+  public string? q5 { get; set; }
+  public string? q6 { get; set; }
+  public string? q7 { get; set; }
+  public string? q8 { get; set; }
+  public string? q9 { get; set; }
+  public string? q10 { get; set; }
+}

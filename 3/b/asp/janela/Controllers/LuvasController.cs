3070ddafbd3a74100df1a675cf7da6ebb09b24d4
Lucas <lucastetsuon@gmail.com>
2023-10-09 08:27:00 -0300

    a

---
+++ 3/b/asp/janela/Controllers/LuvasController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using janela.Models;
+
+namespace janela.Controllers;
+
+public class LuvasController : Controller
+{
+    private readonly ILogger<LuvasController> _logger;
+
+    public LuvasController(ILogger<LuvasController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Signin()
+    {
+        return View();
+    }
+    [HttpPost, ActionName("SignIn")]
+    public IActionResult SigninP(UsersModel model)
+    {
+        return RedirectToAction("ViewUsers", model);
+    }
+    public IActionResult ViewUsers()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ 3/b/asp/janela/Models/UsersModel.cs
@@ -0,0 +1,8 @@
+namespace janela.Models;
+
+public class UsersModel
+{
+    public string? name { get; set; }
+    public string? age { get; set; }
+    public string? profession { get; set; }
+}
